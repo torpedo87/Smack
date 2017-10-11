0eb1c15171086298644b30c96db53f99f80cbfce
torpedo87 <bestofhand@naver.com>
2017-10-11 16:41:45 +0900

    add registerUser api

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
 //segue
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//user defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+//url
+let BASE_URL = "https://samchonchat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
